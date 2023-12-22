d59f2bd37ca4fa7e764315f0492b6ba1bf8c354d
dipendra-wwt <dipendra@worldwebtechnology.com>
2023-12-22 17:03:52 +0530

    Ipoint Initial Commit
    Ipoint Laravel project.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Lydia Caruana Case Study </title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('public/backend/css/style.css') }}">
+</head>
+<body>
+	<!-- innovation section start here -->
+	<section class="innovation-wrap">
+		<div class="container">
+			<div class="innovation-caption flex flex-aln-start">
+				<div class="left-invt-caption">
+					<div class="image-title">
+						<img src="{{ asset('public/backend/images/image5.png') }}" alt="image5">
+						<div class="detail-info">
+							<img class="title-img" src="{{ asset('public/backend/images/Lydia-Caruana.svg') }}" alt="Lydia Caruana">
+							<p>Original photo & AI generated portraits based on this photo.</p>
+						</div>
+					</div>
+					<div class="over-image">
+						<img src="{{ asset('public/backend/images/Portrait1.png') }}" alt="Portrait1">
+					</div>
+				</div>
+				<div class="right-invt-caption">
+					<div class="ctm-heading-text">
+						<div class="ctm-heading-inner">
+							<h6>AI for Innovation</h6>
+							<p>Lorem ipsum dolor sit amet consectetur. Dolor semper in pellentesque ultrices dignissim orci risus aenean viverra.</p>
+						</div>
+					</div>
+					<div class="over-image">
+						<img src="{{ asset('public/backend/images/Portrait2.png') }}" alt="Portrait2">
+					</div>
+					<div class="mid-image">
+						<img src="{{ asset('public/backend/images/Portrait3.png') }}" alt="Portrait3">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- innovation section end here -->
+
+	<!-- Monogram section start here -->
+	<section class="monogram-wrap">
+		<div class="container">
+			<div class="monogram-caption">
+				<div class="lc-logo flex flex-jst-center">
+					<img src="{{ asset('public/backend/images/LC-logo.svg') }}" alt="LC-logo">
+				</div>
+				<div class="titleright-content">
+					<div class="ctm-heading-text">
+						<div class="ctm-heading-inner">
+							<h6>Monogram</h6>
+							<p>Lorem ipsum dolor sit amet consectetur. Dolor semper in pellentesque ultrices dignissim orci risus aenean viverra.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Monogram section end here -->
+
+	<!-- Accent section start here -->
+	<section class="accent-wrap">
+		<div class="container">
+			<div class="accent-caption">
+				<div class="ctm-heading-text">
+					<div class="ctm-heading-inner">
+						<h6>Typography as an Accent</h6>
+						<p>Lorem ipsum dolor sit amet consectetur. Dolor semper in pellentesque ultrices dignissim orci risus aenean viverra.</p>
+					</div>
+				</div>
+				<div class="accent-content-wrap flex flex-aln-start">
+					<div class="accent-content-inner left-caption">
+						<div class="text-img-item">
+							<div class="title-caption flex">
+								<img src="{{ asset('public/backend/images/Roman.svg') }}" alt="Roman">
+							</div>
+							<img class="content-img" src="{{ asset('public/backend/images/roman-text.svg') }}" alt="Roman text">
+						</div>
+
+						<div class="text-img-item">
+							<div class="title-caption flex">
+								<img src="{{ asset('public/backend/images/Italic.svg') }}" alt="Italic">
+							</div>
+							<img class="content-img" src="{{ asset('public/backend/images/italic-text.svg') }}" alt="italic text">
+						</div>
+					</div>
+
+					<div class="accent-content-inner right-caption">
+						<div class="text-img-item">
+							<div class="title-caption flex">
+								<img src="{{ asset('public/backend/images/Bold.svg') }}" alt="Bold">
+							</div>
+							<img class="content-img" src="{{ asset('public/backend/images/bold-text.svg') }}" alt="Bold text">
+						</div>
+
+						<div class="text-img-item">
+							<div class="title-caption flex">
+								<img src="{{ asset('public/backend/images/Regular.svg') }}" alt="Regular">
+							</div>
+							<img class="content-img" src="{{ asset('public/backend/images/regular-text.svg') }}" alt="Regular text">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Accent section end here -->
+
+	<!-- compositions section start here -->
+	<section class="compositions-wrap">
+		<div class="container">
+			<div class="compositions-caption">
+				<div class="compositions-items flex flex-center-aln-s">
+					<div class="compositions-item one">
+						<div class="image-item">
+							<img src="{{ asset('public/backend/images/Lydia1.png') }}" alt="Lydia">
+						</div>
+					</div>
+					<div class="compositions-item two">
+						<div class="image-item">
+							<img src="{{ asset('public/backend/images/Lydia2.png') }}" alt="Lydia">
+						</div>
+					</div>
+					<div class="compositions-item three">
+						<div class="image-item">
+							<img src="{{ asset('public/backend/images/Lydia3.png') }}" alt="Lydia">
+						</div>
+					</div>
+				</div>
+				<div class="titleright-content">
+					<div class="ctm-heading-text">
+						<div class="ctm-heading-inner">
+							<h6>Initial compositions</h6>
+							<p>Lorem ipsum dolor sit amet consectetur. Dolor semper in pellentesque ultrices dignissim orci risus aenean viverra.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- compositions section end here -->
+
+	<!-- Classical singing section start here -->
+	<section class="classical-wrap">
+		<div class="container">
+			<div class="classical-caption">
+				<div class="classical-items flex">
+					 <div class="classical-item">
+					 	<img src="{{ asset('public/backend/images/classical-singing.png') }}" alt="classical singing">
+					 </div>
+					 <div class="classical-item">
+					 	<img src="{{ asset('public/backend/images/classical-singing2.png') }}" alt="classical singing">
+					 </div>
+				</div>
+				<div class="titleright-content">
+					<div class="ctm-heading-text">
+						<div class="ctm-heading-inner">
+							<h6>A/B Testing</h6>
+							<p>Lorem ipsum dolor sit amet consectetur. Dolor semper in pellentesque ultrices dignissim orci risus aenean viverra.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Classical singing section end here -->
+
+	<!-- Vocal lessons section start here -->
+	<section class="v-lessons-wrap">
+		<div class="container">
+			<div class="v-lessons-caption">
+				<div class="v-lessons-items flex">
+					<div class="v-lessons-item">
+						<img src="{{ asset('public/backend/images/v-lessons.png') }}" alt="v-lessons">
+					</div>
+					<div class="v-lessons-item right">
+						<img src="{{ asset('public/backend/images/v-lessons2.png') }}" alt="v-lessons">
+					</div>
+				</div>
+				<div class="titleright-content">
+					<div class="ctm-heading-text">
+						<div class="ctm-heading-inner">
+							<h6>A/B Testing</h6>
+							<p>Lorem ipsum dolor sit amet consectetur. Dolor semper in pellentesque ultrices dignissim orci risus aenean viverra.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Vocal lessons section end here -->
+	<!-- Representation section start here -->
+	<section class="representation-wrap">
+		<div class="container">
+			<div class="representation-caption">
+				<div class="titleright-content">
+					<div class="ctm-heading-text">
+						<div class="ctm-heading-inner">
+							<h6>Final Style Representation</h6>
+							<p>Lorem ipsum dolor sit amet consectetur. Dolor semper in pellentesque ultrices dignissim orci risus aenean viverra.</p>
+						</div>
+					</div>
+				</div>
+				<div class="representation-items flex flex-aln-start">
+					<div class="representation-item tab-item">
+						<img src="{{ asset('public/backend/images/representation-tab.png') }}" alt="representation-tab">
+					</div>
+					<div class="representation-item mobile-item">
+						<img src="{{ asset('public/backend/images/representation-mb.png') }}" alt="representation-mb">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Representation section end here -->
+
+	<!-- thanks for watching section start here -->
+	<section class="watching-wrap"> 
+		<div class="container">
+			<div class="watching-caption flex flex-center">
+				<img src="{{ asset('public/backend/images/thanks-for-watching.svg') }}" alt="Thanks for watching">
+			</div>
+		</div>
+	</section>
+	<!-- thanks for watching section end here -->
+</body>
+</html>
